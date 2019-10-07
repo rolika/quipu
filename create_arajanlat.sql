722baf376dbe32a2939b2fbf692f56c6d53b6d9b
Weisz Roland <weisz.roland@pohlen-dach.hu>
2019-10-07 15:26:00 +0200

    adatbázisok csatolása

--- create_arajanlat.sql
+++ create_arajanlat.sql
@@ -1,16 +1,22 @@
+-- Syntax: SQLite3
+
 PRAGMA foreign_keys = ON;
+
+ATTACH DATABASE "szemely.db" AS "Személy";
+ATTACH DATABASE "szervezet.db" AS "Szervezet";
+ATTACH DATABASE "munka.db" AS "Munka";
 
 -- gdpr-érzékeny
 CREATE TABLE IF NOT EXISTS "KontaktSzemély" (
-    "személy" INTEGER NOT NULL REFERENCES "Személy"("rowid") ON DELETE SET NULL ON UPDATE CASCADE,
-    "cég" INTEGER REFERENCES "Cég"("rowid") ON DELETE SET NULL ON UPDATE CASCADE,
+    "személy" INTEGER NOT NULL REFERENCES "Személy"."Személy"("rowid") ON DELETE SET NULL ON UPDATE CASCADE,
+    "szervezet" INTEGER REFERENCES "Szervezet"."Szervezet"("rowid") ON DELETE SET NULL ON UPDATE CASCADE,
     "megjegyzés" TEXT
 );
 
 -- évente új táblázat készül az ajánlatoknak
 CREATE TABLE IF NOT EXISTS "Árajánlat2019" (
     "szám" TEXT PRIMARY KEY,
-    "munkarész" INTEGER NOT NULL REFERENCES "Munkarész"("rowid") ON DELETE RESTRICT ON UPDATE RESTRICT,
+    "munkarész" INTEGER NOT NULL REFERENCES "Munka"."Munkarész"("rowid") ON DELETE RESTRICT ON UPDATE RESTRICT,
     "kontaktszemély" INTEGER REFERENCES "Kontaktszemély"("rowid") ON DELETE SET NULL ON UPDATE CASCADE,
     "témafelelős" INTEGER REFERENCES "Kontaktszemély"("rowid") ON DELETE RESTRICT ON UPDATE CASCADE,
     "érkezett" DATE DEFAULT (date('now')),
