1fce757bd2dc75ea9e45cadd7b5d0c0058e85c6d
Weisz Roland <weisz.roland@pohlen-dach.hu>
2019-10-07 15:19:58 +0200

    árajánlat-lista

---
+++ create_arajanlat.sql
@@ -0,0 +1,23 @@
+PRAGMA foreign_keys = ON;
+
+-- gdpr-érzékeny
+CREATE TABLE IF NOT EXISTS "KontaktSzemély" (
+    "személy" INTEGER NOT NULL REFERENCES "Személy"("rowid") ON DELETE SET NULL ON UPDATE CASCADE,
+    "cég" INTEGER REFERENCES "Cég"("rowid") ON DELETE SET NULL ON UPDATE CASCADE,
+    "megjegyzés" TEXT
+);
+
+-- évente új táblázat készül az ajánlatoknak
+CREATE TABLE IF NOT EXISTS "Árajánlat2019" (
+    "szám" TEXT PRIMARY KEY,
+    "munkarész" INTEGER NOT NULL REFERENCES "Munkarész"("rowid") ON DELETE RESTRICT ON UPDATE RESTRICT,
+    "kontaktszemély" INTEGER REFERENCES "Kontaktszemély"("rowid") ON DELETE SET NULL ON UPDATE CASCADE,
+    "témafelelős" INTEGER REFERENCES "Kontaktszemély"("rowid") ON DELETE RESTRICT ON UPDATE CASCADE,
+    "érkezett" DATE DEFAULT (date('now')),
+    "leadási határidő" DATE DEFAULT (date('now', '+1 week')),
+    "leadva" DATE,
+    "érvényes" DATE,
+    "ajánlati ár" INTEGER,
+    "esély" INTEGER DEFAULT 5, -- <100%: esély, 100%: nyert,
+    "megjegyzés" TEXT
+);
