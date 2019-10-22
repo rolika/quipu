9080aca1a6c1f33b1de0d4535c6b31b7f0e05ab1
Weisz Roland <weisz.roland@wevik.hu>
2019-10-22 20:04:11 +0200

    személy sqlite-osítva

--- create_szemely.sql
+++ create_szemely.sql
@@ -2,60 +2,60 @@
 
 PRAGMA foreign_keys = ON;
 
--- gdpr-érzékeny
-CREATE TABLE IF NOT EXISTS "Személy" (
-    "előtag" TEXT DEFAULT '',
-    "vezetéknév" TEXT NOT NULL,
-    "keresztnév" TEXT NOT NULL,
-    "nem" TEXT,
-    "érvényes" DATE, -- ha eléri a dátumot, törli a személyt és a hozzá kapcsolódó összes adatot
-    "megjegyzés" TEXT,
-    CONSTRAINT ELL_SZEM_NEM CHECK("nem" in ('férfi', 'nő'))
+CREATE TABLE IF NOT EXISTS Személy (
+    azonosító INTEGER PRIMARY KEY,
+    előtag TEXT DEFAULT '',
+    vezetéknév TEXT NOT NULL,
+    keresztnév TEXT NOT NULL,
+    nem TEXT NOT NULL,
+    megjegyzés TEXT DEFAULT '',
+    CONSTRAINT ELL_SZEM_NEM CHECK(nem in ('férfi', 'nő'))
 );
 
--- gdpr-érzékeny
-CREATE TABLE IF NOT EXISTS "Telefon" (
-    "személy" INTEGER NOT NULL REFERENCES "Személy"("rowid") ON DELETE CASCADE ON UPDATE CASCADE,
-    "telefonszám" TEXT NOT NULL,
-    "megjegyzés" TEXT DEFAULT 'elsődleges'
-);
--- gdpr-érzékeny
-CREATE TABLE IF NOT EXISTS "Email" (
-    "személy" INTEGER NOT NULL REFERENCES "Személy"("rowid") ON DELETE CASCADE ON UPDATE CASCADE,
-    "email-cím" TEXT NOT NULL,
-    "megjegyzés" TEXT DEFAULT 'elsődleges'
+CREATE TABLE IF NOT EXISTS Telefon (
+    személy INTEGER NOT NULL,
+    telefonszám TEXT NOT NULL,
+    megjegyzés TEXT DEFAULT 'elsődleges',
+    FOREIGN KEY(személy) REFERENCES Személy(azonosító) ON DELETE CASCADE
 );
 
--- gdpr-érzékeny
-CREATE TABLE IF NOT EXISTS "Cím" (
-    "személy" INTEGER NOT NULL REFERENCES "Személy"("rowid") ON DELETE CASCADE ON UPDATE CASCADE,
-    "ország" TEXT NOT NULL DEFAULT 'Magyarország',
-    "irányítószám" TEXT,
-    "helység" TEXT NOT NULL,
-    "utca" TEXT, -- házszám is
-    "megjegyzés" TEXT DEFAULT 'elsődleges'
+CREATE TABLE IF NOT EXISTS Email (
+    személy INTEGER NOT NULL,
+    emailcím TEXT NOT NULL,
+    megjegyzés TEXT DEFAULT 'elsődleges',
+    FOREIGN KEY(személy) REFERENCES Személy(azonosító) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS "Megszólítás" (
-    "nem" TEXT PRIMARY KEY REFERENCES "Személy"("nem"),
-    "megszólítás" TEXT
+CREATE TABLE IF NOT EXISTS Cím (
+    személy INTEGER NOT NULL,
+    ország TEXT NOT NULL DEFAULT 'H',
+    irányítószám TEXT DEFAULT '',
+    helység TEXT NOT NULL,
+    utca TEXT DEFAULT '', -- házszám is
+    megjegyzés TEXT DEFAULT 'elsődleges',
+    FOREIGN KEY(személy) REFERENCES Személy(azonosító) ON DELETE CASCADE
 );
-INSERT INTO "Megszólítás" VALUES ("nő", "Hölgyem");
-INSERT INTO "Megszólítás" VALUES ("férfi", "Uram");
 
-CREATE VIEW IF NOT EXISTS "Név"("személy", "név") AS
-    SELECT "rowid", ltrim(printf('%s %s %s', "előtag", "vezetéknév", "keresztnév")) FROM "Személy";
+CREATE TABLE IF NOT EXISTS Megszólítás (
+    nem TEXT,
+    megszólítás TEXT
+);
+INSERT INTO Megszólítás VALUES ("nő", "Hölgyem");
+INSERT INTO Megszólítás VALUES ("férfi", "Uram");
 
-CREATE VIEW IF NOT EXISTS "Teljes cím"("személy", "cím") AS
-    SELECT "személy", printf('%s-%s, %s (%s)', "irányítószám", "helység", "utca", "ország") FROM "Cím";
+CREATE VIEW IF NOT EXISTS Név(személy, név) AS
+    SELECT azonosító, ltrim(printf('%s %s %s', előtag, vezetéknév, keresztnév)) FROM Személy;
 
-CREATE VIEW IF NOT EXISTS "Elérhetőség"("személy", "elérhetőség") AS
-    SELECT "személy", printf('%s: %s, %s', "név", "telefonszám", "email-cím") FROM "Név", "Telefon", "Email"
-        WHERE "személy"="Telefon"."személy"
-            AND "személy"="Email"."személy"
-            AND "Telefon"."megjegyzés"='elsődleges'
-            AND "Email"."megjegyzés"='elsődleges';
+CREATE VIEW IF NOT EXISTS TeljesCím(személy, cím) AS
+    SELECT személy, printf('%s-%s %s, %s', ország, irányítószám, helység, utca) FROM Cím;
 
-CREATE VIEW IF NOT EXISTS "Köszöntés"("személy", "köszöntés") AS
-    SELECT "Személy"."rowid", printf('Tisztelt %s!', "megszólítás") FROM "Személy", "Megszólítás"
-        WHERE "Személy"."nem"="Megszólítás"."nem";
+CREATE VIEW IF NOT EXISTS Elérhetőség(személy, elérhetőség) AS
+    SELECT Név.személy, printf('%s: %s, %s', név, telefonszám, emailcím) FROM Név, Telefon, Email
+        WHERE Név.személy=Telefon.személy
+            AND Név.személy=Email.személy
+            AND Telefon.megjegyzés='elsődleges'
+            AND Email.megjegyzés='elsődleges';
+
+CREATE VIEW IF NOT EXISTS Köszöntés(személy, köszöntés) AS
+    SELECT Személy.azonosító, printf('Tisztelt %s!', megszólítás) FROM Személy, Megszólítás
+        WHERE Személy.nem=Megszólítás.nem;
