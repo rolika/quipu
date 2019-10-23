2a1814242a880789ae2be072b29f9e4c9e87143c
Weisz Roland <weisz.roland@wevik.hu>
2019-10-23 08:36:29 +0200

    ékezetlen személyek

--- create_szemely.sql
+++ create_szemely.sql
@@ -2,60 +2,69 @@
 
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE IF NOT EXISTS Személy (
-    azonosító INTEGER PRIMARY KEY,
-    előtag TEXT DEFAULT '',
-    vezetéknév TEXT NOT NULL,
-    keresztnév TEXT NOT NULL,
-    nem TEXT NOT NULL,
-    megjegyzés TEXT DEFAULT '',
-    CONSTRAINT ELL_SZEM_NEM CHECK(nem in ('férfi', 'nő'))
+
+CREATE TABLE IF NOT EXISTS megszolitas (
+    nem TEXT PRIMARY KEY,
+    megszolitas TEXT
+);
+INSERT INTO Megszolitas VALUES ("nő", "Hölgyem");
+INSERT INTO Megszolitas VALUES ("férfi", "Uram");
+
+
+CREATE TABLE IF NOT EXISTS szemely (
+    azonosito INTEGER PRIMARY KEY,
+    elotag TEXT DEFAULT '',
+    vezeteknev TEXT NOT NULL,
+    keresztnev TEXT NOT NULL,
+    nem TEXT NOT NULL REFERENCES megszolitas,
+    megjegyzes TEXT DEFAULT ''
 );
 
-CREATE TABLE IF NOT EXISTS Telefon (
-    személy INTEGER NOT NULL,
-    telefonszám TEXT NOT NULL,
-    megjegyzés TEXT DEFAULT 'elsődleges',
-    FOREIGN KEY(személy) REFERENCES Személy(azonosító) ON DELETE CASCADE
+
+CREATE TABLE IF NOT EXISTS telefon (
+    szemely INTEGER NOT NULL REFERENCES szemely ON DELETE CASCADE,
+    telefonszam TEXT NOT NULL,
+    megjegyzes TEXT DEFAULT 'elsődleges'
 );
 
-CREATE TABLE IF NOT EXISTS Email (
-    személy INTEGER NOT NULL,
-    emailcím TEXT NOT NULL,
-    megjegyzés TEXT DEFAULT 'elsődleges',
-    FOREIGN KEY(személy) REFERENCES Személy(azonosító) ON DELETE CASCADE
+
+CREATE TABLE IF NOT EXISTS email (
+    szemely INTEGER NOT NULL REFERENCES szemely ON DELETE CASCADE,
+    emailcim TEXT NOT NULL,
+    megjegyzes TEXT DEFAULT 'elsődleges'
 );
 
-CREATE TABLE IF NOT EXISTS Cím (
-    személy INTEGER NOT NULL,
-    ország TEXT NOT NULL DEFAULT 'H',
-    irányítószám TEXT DEFAULT '',
-    helység TEXT NOT NULL,
+
+CREATE TABLE IF NOT EXISTS cim (
+    szemely INTEGER NOT NULL REFERENCES szemely ON DELETE CASCADE,
+    orszag TEXT NOT NULL DEFAULT 'H',
+    iranyitoszam TEXT DEFAULT '',
+    helyseg TEXT NOT NULL,
     utca TEXT DEFAULT '', -- házszám is
-    megjegyzés TEXT DEFAULT 'elsődleges',
-    FOREIGN KEY(személy) REFERENCES Személy(azonosító) ON DELETE CASCADE
+    megjegyzes TEXT DEFAULT 'elsődleges'
 );
 
-CREATE TABLE IF NOT EXISTS Megszólítás (
-    nem TEXT,
-    megszólítás TEXT
-);
-INSERT INTO Megszólítás VALUES ("nő", "Hölgyem");
-INSERT INTO Megszólítás VALUES ("férfi", "Uram");
 
-CREATE VIEW IF NOT EXISTS Név(személy, név) AS
-    SELECT azonosító, ltrim(printf('%s %s %s', előtag, vezetéknév, keresztnév)) FROM Személy;
+CREATE VIEW IF NOT EXISTS nev(szemely, nev) AS
+    SELECT azonosito, ltrim(printf('%s %s %s', elotag, vezeteknev, keresztnev))
+        FROM szemely;
 
-CREATE VIEW IF NOT EXISTS TeljesCím(személy, cím) AS
-    SELECT személy, printf('%s-%s %s, %s', ország, irányítószám, helység, utca) FROM Cím;
 
-CREATE VIEW IF NOT EXISTS Elérhetőség(személy, elérhetőség) AS
-    SELECT Név.személy, printf('%s: %s, %s', név, telefonszám, emailcím) FROM Név, Telefon, Email
-        WHERE Név.személy=Telefon.személy
-            AND Név.személy=Email.személy
-            AND Telefon.megjegyzés='elsődleges'
-            AND Email.megjegyzés='elsődleges';
+CREATE VIEW IF NOT EXISTS teljescim(szemely, cim) AS
+    SELECT szemely, printf('%s-%s %s, %s', orszag, iranyitoszam, helyseg, utca)
+        FROM cim;
 
-CREATE VIEW IF NOT EXISTS Köszöntés(személy, köszöntés) AS
-    SELECT Személy.azonosító, printf('Tisztelt %s!', megszólítás) FROM Személy, Megszólítás
-        WHERE Személy.nem=Megszólítás.nem;
+
+CREATE VIEW IF NOT EXISTS elerhetoseg(szemely, elerhetoseg) AS
+    SELECT nev.szemely, printf('%s: %s, %s', nev, telefonszam, emailcim)
+        FROM nev, telefon, email
+            WHERE nev.szemely=telefon.szemely
+                AND nev.szemely=email.szemely
+                AND telefon.megjegyzes='elsődleges'
+                AND email.megjegyzes='elsődleges';
+
+
+CREATE VIEW IF NOT EXISTS koszontes(szemely, koszontes) AS
+    SELECT szemely.azonosito, printf('Tisztelt %s!', megszolitas)
+        FROM szemely, megszolitas
+            WHERE szemely.nem=megszolitas.nem;
