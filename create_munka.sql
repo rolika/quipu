7ae47b8523d3b065452bd5228267b6e57c13663b
Weisz Roland <weisz.roland@wevik.hu>
2019-10-24 19:23:09 +0200

    munka sqlite-osítva

--- create_munka.sql
+++ create_munka.sql
@@ -2,33 +2,36 @@
 
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE IF NOT EXISTS "Munka" (
-    "megnevezés" TEXT NOT NULL,
-    "megjegyzés" TEXT
-    
-);
 
-CREATE TABLE IF NOT EXISTS "Jelleg" (
-    "munka" INTEGER NOT NULL REFERENCES "Munka"("rowid"),
-    "jelleg" TEXT NOT NULL DEFAULT 'új',
-    "megjegyzés" TEXT,
-    CONSTRAINT ELL_MNK_JLG CHECK("jelleg" in ('új', 'felújítás', 'bővítés', 'átalakítás', 'bérbeadás', 'eladás'))
+CREATE TABLE IF NOT EXISTS projekt (
+    azonosito INTEGER PRIMARY KEY,
+    megnevezes TEXT NOT NULL,
+    megjegyzes TEXT DEFAULT ''
 );
 
 
-CREATE TABLE IF NOT EXISTS "Cím" (
-    "munka" INTEGER NOT NULL REFERENCES "Munka"("rowid"),
-    "ország" TEXT NOT NULL DEFAULT 'Magyarország',
-    "megye" TEXT,
-    "irányítószám" TEXT,
-    "helység" TEXT NOT NULL,
-    "utca" TEXT, -- házszám is
-    "hrsz" TEXT,
-    "megjegyzés" TEXT
+CREATE TABLE IF NOT EXISTS munkaresz (
+    projekt INTEGER NOT NULL REFERENCES projekt,
+    megnevezes TEXT NOT NULL DEFAULT 'szigetelés',
+    megjegyzes TEXT DEFAULT ''
 );
 
-CREATE TABLE IF NOT EXISTS "Munkarész" (
-    "munka" INTEGER NOT NULL REFERENCES "Munka"("rowid"),
-    "megnevezés" TEXT NOT NULL DEFAULT 'hőszigetelés'
-    "megjegyzés" TEXT
+
+CREATE TABLE IF NOT EXISTS cim (
+    projekt INTEGER NOT NULL REFERENCES projekt,
+    orszag TEXT NOT NULL DEFAULT 'H',
+    megye TEXT NOT NULL DEFAULT '',
+    iranyitoszam TEXT DEFAULT '',
+    helyseg TEXT NOT NULL,
+    utca TEXT DEFAULT '', -- házszám is
+    hrsz TEXT DEFAULT '',
+    megjegyzes TEXT DEFAULT ''
+);
+
+
+CREATE TABLE IF NOT EXISTS jelleg (
+    projekt INTEGER NOT NULL REFERENCES projekt,
+    megnevezes TEXT NOT NULL DEFAULT 'új',
+    megjegyzes TEXT DEFAULT '',
+    CONSTRAINT ELL_JLG CHECK(megnevezes in ('új', 'felújítás', 'bővítés', 'átalakítás', 'bérbeadás', 'értékesítés'))
 );
