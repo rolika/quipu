ced9ab81c42ae8ab934a34289ab4b8210008d4a4
Weisz Roland <weisz.roland@wevik.hu>
2020-02-23 12:20:26 +0100

    felesleges view-k törölve

--- scripts/quipu.py
+++ scripts/quipu.py
@@ -65,29 +65,6 @@ class Quipu(Frame):
             szemely="INTEGER NOT NULL REFERENCES szemely", szervezet="INTEGER", megjegyzes="TEXT")
 
         self.szemely_kon.executescript("""
-            PRAGMA foreign_keys = ON;
-
-            CREATE VIEW IF NOT EXISTS nev(szemely, nev) AS
-                SELECT azonosito, ltrim(printf('%s %s, %s', vezeteknev, keresztnev, elotag))
-                    FROM szemely;
-
-            CREATE VIEW IF NOT EXISTS teljescim(szemely, cim) AS
-                SELECT szemely, printf('%s-%s %s, %s', orszag, iranyitoszam, helyseg, utca)
-                    FROM cim;
-
-            CREATE VIEW IF NOT EXISTS elerhetoseg(szemely, elerhetoseg) AS
-                SELECT nev.szemely, printf('%s: %s, %s', nev, telefonszam, emailcim)
-                    FROM nev, telefon, email
-                        WHERE nev.szemely=telefon.szemely
-                            AND nev.szemely=email.szemely
-                            AND telefon.megjegyzes='elsődleges'
-                            AND email.megjegyzes='elsődleges';
-
-            CREATE VIEW IF NOT EXISTS koszontes(szemely, koszontes) AS
-                SELECT szemely.azonosito, printf('Tisztelt %s!', megszolitas)
-                    FROM szemely, megszolitas
-                        WHERE szemely.nem=megszolitas.nem;
-
             -- automatikusan add hozzá a kontaktszemélyhez is
             CREATE TRIGGER IF NOT EXISTS kntkt AFTER INSERT ON szemely
                 BEGIN
