72571b42c3dedb751fb89db20831b8ac0bd77b90
Weisz Roland <weisz.roland@wevik.hu>
2021-03-24 18:44:41 +0100

    dolog helyett csomó

--- scripts/csomo.py
+++ scripts/csomo.py
@@ -1,55 +1,61 @@
 import copy
 
 
-class Dolog:
-    """Nyilvántartott dolgok alaposztálya."""
+class Csomo:
+    """A kipu egy csomóírás. Ez az alkalmazás is alapvető csomókból áll."""
     def __init__(self):
         self._adatok = dict()
         self._tabla = None
 
     def __str__(self):
-        """Adatok megjelenítése, elsősorban debugoláshoz"""
+        """Csomó szöveges megjelenítése, elsősorban debugoláshoz."""
         raise NotImplementedError
 
     def __repr__(self):
-        """Név megjelenítése sorbarendezéshez"""
+        """Csomó neve sorbarendezéshez, jellemzően kisbetűs, ékezetek nélkül, ld. self._ascii_rep()."""
         raise NotImplementedError
 
     def __bool__(self):
-        """Dolog elegendően meghatározott-e."""
+        """A csomó elegendően meghatározott-e, azaz a felhasználó elég adatot adott meg vagy sem."""
         raise NotImplementedError
 
     @property
     def azonosito(self):
+        """A csomó azonosítója (SQL PRIMARY KEY)."""
         return self._adatok.get("azonosito")
     
     @azonosito.setter
     def azonosito(self, azonosito):
+        """A csomó azonosítójának (SQL PRIMARY KEY) beállítása kívülről."""
         self._adatok["azonosito"] = azonosito
 
     @property
     def megjegyzes(self):
+        """Minden csomóhoz fűzhető valamilyen megjegyzés."""
         return self._adatok.get("megjegyzes")
 
     def listanezet(self):
-        """Dolog megjelenítése kiválasztáshoz (Combobox)"""
+        """Csomó szöveges megjelenítése kiválasztáshoz (pl. Combobox)."""
         raise NotImplementedError
 
     def meglevo(self, kon):
-        """Ellenőrzi, hogy a dolog szerepel-e az adatbázisban"""
+        """Ellenőrzi, hogy a csomó szerepel-e az adatbázisban.
+        kon:    tamer modul adatbázis konnektora"""
         adatok = copy.copy(self._adatok)  # shallow copy
         adatok.pop("azonosito", None)
         return kon.select(self._tabla, logic="AND", **adatok).fetchone()
 
     def ment(self, kon):
-        """Menti vagy módosítja a dolog adatait"""
+        """Menti vagy módosítja a csomó adatait.
+        kon:    tamer modul adatbázis konnektora"""
         if self.azonosito:
             return kon.update(self._tabla, self._adatok, azonosito=self.azonosito)  # True vagy False
         else:
             return kon.insert(self._tabla, **self._adatok)  # lastrowid vagy None
 
     def torol(self, kon):
-        """Törli az adatbázisból a dolog-bejegyzést"""
+        """Törli az adatbázisból a csomót.
+        kon:    tamer modul adatbázis konnektora"""
         return kon.delete(self._tabla, azonosito=self.azonosito)
 
     def _ascii_rep(self, szoveg):
@@ -58,5 +64,6 @@ class Dolog:
         return szoveg.lower().translate(str.maketrans("áéíóöőúüű", "aeiooouuu"))
 
     def _nullazo(self, attr):
-        """Ha hiányzik az adat, nem írjuk ki egyáltalán."""
+        """Ha hiányzik az adat, nem írjuk ki egyáltalán.
+        attr:   attribútum, vagy annak hiánya, ha üres"""
         return " ({})".format(attr) if attr else ""
