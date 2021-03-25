cffc2cadf760a21a77b369033f1187023c31a08b
Weisz Roland <weisz.roland@wevik.hu>
2021-03-25 18:47:57 +0100

    előző kód törölve

--- scripts/test_ajanlat.py
+++ scripts/test_ajanlat.py
@@ -114,48 +114,4 @@ class ProjektlistaTest(unittest.TestCase):
                         if not ajanlat.meglevo(self._ajanlat_kon):
                             ajanlat.ment(self._ajanlat_kon)
 
-
-                """ # személyek
-                vnev, knev = projekt.szemely.split(" ", maxsplit=1)
-                szemely = Szemely(vezeteknev=vnev, keresztnev=knev, megjegyzes="")
-                if szemely.meglevo(self._szemely_kon):
-                    print("{} már meglévő személy.". format(szemely))
-                else:
-                    szemely_id = szemely.ment(self._szemely_kon)
-                    # telefonszám
-                    telefon = Telefon(szemely=szemely_id, telefonszam=projekt.telefonszam, megjegyzes="")
-                    email = Email(szemely=szemely_id, emailcim=projekt.emailcim, megjegyzes="")
-                    if telefon.meglevo(self._szemely_kon) or email.meglevo(self._szemely_kon):
-                        print("{} már meglévő telefonszám/email-cím.".format(telefon))
-                    else:
-                        telefon.ment(self._szemely_kon)
-                        email.ment(self._szemely_kon)
-                # szervezetek
-                szervezet = Szervezet(rovidnev=projekt.szervezet, teljesnev="", megjegyzes="", gyakorisag=0)
-                szervezet_id = szervezet.ment(self._szervezet_kon) if bool(szervezet) else Kulcs.MAGANSZEMELY.kulcs
-                # kontakt
-                if szemely_id:
-                    kontakt = Kontakt(szemely=szemely_id, szervezet=szervezet_id, megjegyzes="")
-                    if not kontakt.meglevo(self._kontakt_kon):
-                        kontakt_id = kontakt.ment(self._kontakt_kon)
-                # projekt
-                ev, szam = projekt.szam.split("/")
-                projekt_ = Projekt(ev=ev, szam=szam, megnevezes=projekt.nev, rovidnev="", megjegyzes="")
-                if bool(projekt_) and not projekt_.meglevo(self._projekt_kon):
-                    projekt_id = projekt_.ment(self._projekt_kon)
-                # munkarész
-                if projekt_id:
-                    munkaresz = Munkaresz(projekt=projekt_id, megnevezes="szigetelés", enaplo=1, megjegyzes="")
-                    munkaresz_id = munkaresz.ment(self._projekt_kon)
-                # ajánlatkérés
-                if kontakt_id and munkaresz_id:
-                    ajanlatkeres = Ajanlatkeres(munkaresz=munkaresz_id, ajanlatkero=kontakt_id, temafelelos=1, megjegyzes="")
-                    if not ajanlatkeres.meglevo(self._ajanlat_kon):
-                        ajanlatkeres_id = ajanlatkeres.ment(self._ajanlat_kon)
-                # ajanlat
-                if ajanlatkeres_id:
-                    ajanlat = Ajanlat(ajanlatkeres=ajanlatkeres_id, ajanlatiar=projekt.ar, megjegyzes="")
-                    if not ajanlat.meglevo(self._ajanlat_kon):
-                        ajanlat.ment(self._ajanlat_kon) """
-
         return True
