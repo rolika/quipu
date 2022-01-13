5490372d989e8efd03214e7b6ca012f93dfaed62
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-01-13 20:19:05 +0100

    új személy mentése

--- src/app.py
+++ src/app.py
@@ -1,14 +1,20 @@
 from pathlib import Path
 import bottle
+from quipu import Quipu
 
+from szemely import Szemely
 
-btl = bottle.Bottle()
 
 cwd = Path.cwd()  # current directory
 html_path = cwd / "html"
 img_path = cwd / "img"
 css_path = cwd / "css"
+db_path = cwd / "db"
 tpl_path = cwd / "tpl"
+
+
+btl = bottle.Bottle()
+kipu = Quipu(db_path)
 
 
 @btl.route("/")
@@ -31,9 +37,16 @@ def insider():
     return bottle.static_file("insider.html", root=html_path)
 
 
-@btl.route("/insider/<csomo>")
-def csomo(csomo):
-    return bottle.static_file(csomo + "form.html", root=html_path)
+@btl.get("/insider/szemely")
+def uj_szemely():
+    req = bottle.request.query
+    if req.save:
+        del req["save"]
+        szemely = Szemely(kon=kipu.kon, **req)
+        uj_id = szemely.ment()
+        return "<p>{} elmentve, {} azonosítószám alatt.</p>".format(str(szemely), uj_id)
+    else:
+        return bottle.static_file("szemelyform.html", root=html_path)
 
 
 btl.run(reloader=True, debug=True)
--- src/quipu.py
+++ src/quipu.py
@@ -37,21 +37,23 @@ class Quipu:
     A kipukon rögzített értékeket meglepő módon egy kettes számrendszeren alapuló, kövek helyzetével operáló, számoló
     eszközzel, egy ősi számítógéppel dolgozták fel. [Wikipedia nyomán]
     """
-    def __init__(self) -> None:
+    def __init__(self, db_path) -> None:
+
+        self._db_path = db_path
 
         # adatbázis konnektorok
-        kon = Konnektor(
-            szemely=self._init_szemely_db("szemely"),
-            szervezet=self._init_szervezet_db("szervezet"),
-            kontakt=self._init_kontakt_db("kontakt"),
-            projekt=self._init_projekt_db("projekt"),
-            ajanlat=self._init_ajanlat_db("ajanlat"),
-            raktar=self._init_raktar_db("raktar")
+        self.kon = Konnektor(
+            szemely=self._init_szemely_db("szemely")
+            # szervezet=self._init_szervezet_db("szervezet"),
+            # kontakt=self._init_kontakt_db("kontakt"),
+            # projekt=self._init_projekt_db("projekt"),
+            # ajanlat=self._init_ajanlat_db("ajanlat"),
+            # raktar=self._init_raktar_db("raktar")
             )
 
     def _init_szemely_db(self, db_file:str) -> tamer.Tamer:
         """Személy adatbázis inicializálása"""
-        szemely_kon = tamer.Tamer("db/" + db_file + ".db")
+        szemely_kon = tamer.Tamer("szemely.db")
 
         szemely_kon.create("szemely",
             azonosito="INTEGER PRIMARY KEY",
@@ -65,205 +67,205 @@ class Quipu:
 
         return szemely_kon
 
-    def _init_szervezet_db(self, db_file:str) -> tamer.Tamer:
-        """Szervezet adatbázis inicializálása"""
-        szervezet_kon = tamer.Tamer("db/" + db_file + ".db")
+    # def _init_szervezet_db(self, db_file:str) -> tamer.Tamer:
+    #     """Szervezet adatbázis inicializálása"""
+    #     szervezet_kon = tamer.Tamer("db/" + db_file + ".db")
 
-        szervezet_kon.create("szervezet",
-            azonosito="INTEGER PRIMARY KEY",
-            rovidnev="TEXT NOT NULL",
-            teljesnev="TEXT",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     szervezet_kon.create("szervezet",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         rovidnev="TEXT NOT NULL",
+    #         teljesnev="TEXT",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        return szervezet_kon
+    #     return szervezet_kon
 
-    def _init_kontakt_db(self, db_file:str) -> tamer.Tamer:
-        """Kontaktszemélyek adatbázisának inicializálása"""
-        kontakt_kon = tamer.Tamer("db/" + db_file + ".db")
+    # def _init_kontakt_db(self, db_file:str) -> tamer.Tamer:
+    #     """Kontaktszemélyek adatbázisának inicializálása"""
+    #     kontakt_kon = tamer.Tamer("db/" + db_file + ".db")
 
-        kontakt_kon.create("kontakt",
-            azonosito="INTEGER PRIMARY KEY",
-            szemely="INTEGER",
-            szervezet="INTEGER",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     kontakt_kon.create("kontakt",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         szemely="INTEGER",
+    #         szervezet="INTEGER",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        kontakt_kon.create("telefon",
-            azonosito="INTEGER PRIMARY KEY",
-            szervezet="INTEGER NOT NULL REFERENCES kontakt",
-            telefonszam="TEXT NOT NULL",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     kontakt_kon.create("telefon",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         szervezet="INTEGER NOT NULL REFERENCES kontakt",
+    #         telefonszam="TEXT NOT NULL",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        kontakt_kon.create("email",
-            azonosito="INTEGER PRIMARY KEY",
-            szervezet="INTEGER NOT NULL REFERENCES kontakt",
-            emailcim="TEXT NOT NULL",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     kontakt_kon.create("email",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         szervezet="INTEGER NOT NULL REFERENCES kontakt",
+    #         emailcim="TEXT NOT NULL",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        kontakt_kon.create("cim",
-            azonosito="INTEGER PRIMARY KEY",
-            szervezet="INTEGER NOT NULL REFERENCES kontakt",
-            orszag="TEXT DEFAULT 'H'",
-            megye="TEXT DEFAULT ''",
-            iranyitoszam="TEXT DEFAULT ''",
-            helyseg="TEXT NOT NULL",
-            utca="TEXT DEFAULT ''",
-            hrsz="TEXT DEFAULT ''",
-            postafiok="TEXT DEFAULT ''",
-            honlap="TEXT DEFAULT ''",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     kontakt_kon.create("cim",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         szervezet="INTEGER NOT NULL REFERENCES kontakt",
+    #         orszag="TEXT DEFAULT 'H'",
+    #         megye="TEXT DEFAULT ''",
+    #         iranyitoszam="TEXT DEFAULT ''",
+    #         helyseg="TEXT NOT NULL",
+    #         utca="TEXT DEFAULT ''",
+    #         hrsz="TEXT DEFAULT ''",
+    #         postafiok="TEXT DEFAULT ''",
+    #         honlap="TEXT DEFAULT ''",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        kontakt_kon.create("vevo",
-            azonosito="INTEGER PRIMARY KEY",
-            kontakt="INTEGER UNIQUE REFERENCES kontakt",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     kontakt_kon.create("vevo",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         kontakt="INTEGER UNIQUE REFERENCES kontakt",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        kontakt_kon.create("szallito",
-            azonosito="INTEGER PRIMARY KEY",
-            kontakt="INTEGER UNIQUE REFERENCES kontakt",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     kontakt_kon.create("szallito",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         kontakt="INTEGER UNIQUE REFERENCES kontakt",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        kontakt_kon.create("gyarto",
-            azonosito="INTEGER PRIMARY KEY",
-            kontakt="INTEGER UNIQUE REFERENCES kontakt",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     kontakt_kon.create("gyarto",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         kontakt="INTEGER UNIQUE REFERENCES kontakt",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        return kontakt_kon
+    #     return kontakt_kon
 
-    def _init_projekt_db(self, db_file:str) -> tamer.Tamer:
-        """Projekt adatbázis inicializálása"""
-        projekt_kon = tamer.Tamer("db/" + db_file + ".db")
+    # def _init_projekt_db(self, db_file:str) -> tamer.Tamer:
+    #     """Projekt adatbázis inicializálása"""
+    #     projekt_kon = tamer.Tamer("db/" + db_file + ".db")
 
-        projekt_kon.create("projekt",
-            azonosito="INTEGER PRIMARY KEY",
-            megnevezes="TEXT NOT NULL",
-            rovidnev="TEXT",
-            ev="INTEGER NOT NULL",
-            szam="INTEGER NOT NULL",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     projekt_kon.create("projekt",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         megnevezes="TEXT NOT NULL",
+    #         rovidnev="TEXT",
+    #         ev="INTEGER NOT NULL",
+    #         szam="INTEGER NOT NULL",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        projekt_kon.create("munkaresz",
-            azonosito="INTEGER PRIMARY KEY",
-            projekt="INTEGER NOT NULL REFERENCES projekt",
-            megnevezes="TEXT NOT NULL",
-            enaplo="INTEGER",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     projekt_kon.create("munkaresz",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         projekt="INTEGER NOT NULL REFERENCES projekt",
+    #         megnevezes="TEXT NOT NULL",
+    #         enaplo="INTEGER",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        projekt_kon.create("cim",
-            azonosito="INTEGER PRIMARY KEY",
-            munkaresz="INTEGER NOT NULL REFERENCES munkaresz",
-            orszag="TEXT DEFAULT 'H'",
-            megye="TEXT DEFAULT ''",
-            iranyitoszam="TEXT DEFAULT ''",
-            helyseg="TEXT NOT NULL",
-            utca="TEXT DEFAULT ''",
-            hrsz="TEXT DEFAULT ''",
-            postafiok="TEXT DEFAULT ''",
-            honlap="TEXT DEFAULT ''",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     projekt_kon.create("cim",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         munkaresz="INTEGER NOT NULL REFERENCES munkaresz",
+    #         orszag="TEXT DEFAULT 'H'",
+    #         megye="TEXT DEFAULT ''",
+    #         iranyitoszam="TEXT DEFAULT ''",
+    #         helyseg="TEXT NOT NULL",
+    #         utca="TEXT DEFAULT ''",
+    #         hrsz="TEXT DEFAULT ''",
+    #         postafiok="TEXT DEFAULT ''",
+    #         honlap="TEXT DEFAULT ''",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        projekt_kon.create("jelleg",
-            azonosito="INTEGER PRIMARY KEY",
-            munkaresz="INTEGER NOT NULL REFERENCES munkaresz",
-            megnevezes="TEXT NOT NULL",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     projekt_kon.create("jelleg",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         munkaresz="INTEGER NOT NULL REFERENCES munkaresz",
+    #         megnevezes="TEXT NOT NULL",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        return projekt_kon
+    #     return projekt_kon
 
-    def _init_ajanlat_db(self, db_file:str) -> tamer.Tamer:
-        """Ajánlat adatbázis inicializálása."""
-        ajanlat_kon = tamer.Tamer("db/" + db_file + ".db")
+    # def _init_ajanlat_db(self, db_file:str) -> tamer.Tamer:
+    #     """Ajánlat adatbázis inicializálása."""
+    #     ajanlat_kon = tamer.Tamer("db/" + db_file + ".db")
 
-        ajanlat_kon.create("ajanlatkeres",
-            azonosito="INTEGER PRIMARY KEY",
-            jelleg="INTEGER NOT NULL",
-            ajanlatkero="INTEGER NOT NULL",
-            temafelelos="INTEGER NOT NULL",
-            erkezett="TEXT DEFAULT CURRENT_DATE",
-            hatarido="TEXT DEFAULT ''",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     ajanlat_kon.create("ajanlatkeres",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         jelleg="INTEGER NOT NULL",
+    #         ajanlatkero="INTEGER NOT NULL",
+    #         temafelelos="INTEGER NOT NULL",
+    #         erkezett="TEXT DEFAULT CURRENT_DATE",
+    #         hatarido="TEXT DEFAULT ''",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        ajanlat_kon.create("ajanlat",
-            azonosito="INTEGER PRIMARY KEY",
-            ajanlatkeres="INTEGER NOT NULL REFERENCES ajanlatkeres",
-            ajanlatiar="INTEGER NOT NULL",
-            leadva="TEXT DEFAULT CURRENT_DATE",
-            ervenyes="TEXT DEFAULT ''",
-            esely="INTEGER DEFAULT 5",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT DEFAULT ''")
+    #     ajanlat_kon.create("ajanlat",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         ajanlatkeres="INTEGER NOT NULL REFERENCES ajanlatkeres",
+    #         ajanlatiar="INTEGER NOT NULL",
+    #         leadva="TEXT DEFAULT CURRENT_DATE",
+    #         ervenyes="TEXT DEFAULT ''",
+    #         esely="INTEGER DEFAULT 5",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT DEFAULT ''")
 
-        return ajanlat_kon
+    #     return ajanlat_kon
 
-    def _init_raktar_db(self, db_file:str) -> tamer.Tamer:
-        """Raktárkészlet-adatbázis inicializálása."""
-        raktar_kon = tamer.Tamer("db/" + db_file + ".db")
+    # def _init_raktar_db(self, db_file:str) -> tamer.Tamer:
+    #     """Raktárkészlet-adatbázis inicializálása."""
+    #     raktar_kon = tamer.Tamer("db/" + db_file + ".db")
 
-        raktar_kon.create("anyag",
-            azonosito="INTEGER PRIMARY KEY",
-            gyarto="INTEGER",
-            nev="TEXT",
-            tipus="TEXT",
-            cikkszam="TEXT",
-            leiras="TEXT",
-            szin="TEXT",
-            szinkod = "TEXT",
-            egyseg="TEXT",
-            kiszereles_nev="TEXT",
-            kiszereles="REAL",
-            csomagolas_nev="TEXT",
-            csomagolas="REAL",
-            kritikus="REAL",
-            szallitasi_ido="INTEGER",
-            eltarthato="INTEGER DEFAULT 0",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT")
+    #     raktar_kon.create("anyag",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         gyarto="INTEGER",
+    #         nev="TEXT",
+    #         tipus="TEXT",
+    #         cikkszam="TEXT",
+    #         leiras="TEXT",
+    #         szin="TEXT",
+    #         szinkod = "TEXT",
+    #         egyseg="TEXT",
+    #         kiszereles_nev="TEXT",
+    #         kiszereles="REAL",
+    #         csomagolas_nev="TEXT",
+    #         csomagolas="REAL",
+    #         kritikus="REAL",
+    #         szallitasi_ido="INTEGER",
+    #         eltarthato="INTEGER DEFAULT 0",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT")
 
-        raktar_kon.create("termek",
-            azonosito="INTEGER PRIMARY KEY",
-            anyag="INTEGER",
-            szallito="INTEGER",
-            egysegar="INTEGER",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT")
+    #     raktar_kon.create("termek",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         anyag="INTEGER",
+    #         szallito="INTEGER",
+    #         egysegar="INTEGER",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT")
 
-        raktar_kon.create("keszlet",
-            azonosito="INTEGER PRIMARY KEY",
-            termek="INTEGER",
-            mennyiseg="REAL",
-            erkezett="DATE",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT")
+    #     raktar_kon.create("keszlet",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         termek="INTEGER",
+    #         mennyiseg="REAL",
+    #         erkezett="DATE",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT")
         
-        raktar_kon.create("szallitolevel",
-            azonosito="INTEGER PRIMARY KEY",
-            jelleg="INTEGER",
-            szam="TEXT",
-            datum="DATETIME",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT")
+    #     raktar_kon.create("szallitolevel",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         jelleg="INTEGER",
+    #         szam="TEXT",
+    #         datum="DATETIME",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT")
 
-        raktar_kon.create("tetel",
-            azonosito="INTEGER PRIMARY KEY",
-            szallitolevel="INTEGER",
-            aru="INTEGER",
-            mennyiseg="REAL",
-            gyakorisag="INTEGER DEFAULT 0",
-            megjegyzes="TEXT")
+    #     raktar_kon.create("tetel",
+    #         azonosito="INTEGER PRIMARY KEY",
+    #         szallitolevel="INTEGER",
+    #         aru="INTEGER",
+    #         mennyiseg="REAL",
+    #         gyakorisag="INTEGER DEFAULT 0",
+    #         megjegyzes="TEXT")
         
-        return raktar_kon
+    #     return raktar_kon
 
 
 if __name__ == "__main__":
