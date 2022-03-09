5d698f25fc21ff6af36cfec8dd8fc3bf075df5bc
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-03-09 20:52:28 +0100

    a konnektor egy dict

--- scr/konnektor.py
+++ scr/konnektor.py
@@ -5,18 +5,13 @@ import json
 import tamer
 
 
-class Konnektor:
+class Konnektor(dict):
     """Adatbázis kapcsolat gyűjtőosztálya.
     Ha még nem léteznek, létrehozza az adatbázis file-okat.
-    A külső használat úgy fog kinézni, hogy pl. kon.projekt.select(...)"""
-    def __init__(self, **kwargs) -> None:
-        """Konnektor inicializása.
-        kwargs: név=konnektor párosok
-            A név egy valid Python változó legyen, ami célszerűen azonos az adatbázis filenevekkel.
-            A konnektor pedig az adatbázis-file csatlakozásánák hivatkozása.
-        """
+    A külső használat úgy fog kinézni, hogy pl. kon.projekt.select(...) vagy kon["projekt"].execute(...)"""
+    def __init__(self) -> None:
+        """Konnektor inicializása."""
         db_path = pathlib.Path("db/")
-        self._kon = dict()
 
         with open(pathlib.Path("sql/sql_create.json")) as f:
             self._db_struktura = json.load(f)
@@ -29,16 +24,16 @@ class Konnektor:
         
         for db_nev in self._db_struktura:
             print("Kapcsolódás adatbázishoz:", db_nev)
-            self._kon[db_nev] = tamer.Tamer(db_path / (db_nev+".db"))
+            self[db_nev] = tamer.Tamer(db_path / (db_nev+".db"))
             if db_nev in ("projekt", "raktar"):
-                kontakt_kapcsolt = self._kon[db_nev].attach(kontakt=str(db_path / "kontakt.db"))
+                kontakt_kapcsolt = self[db_nev].attach(kontakt=str(db_path / "kontakt.db"))
             else:
                 kontakt_kapcsolt = False
             for tabla, oszlopok in self._db_struktura[db_nev].items():
                 self._db_struktura[db_nev][tabla]
-                self._kon[db_nev].create(tabla, **oszlopok)
+                self[db_nev].create(tabla, **oszlopok)
             if kontakt_kapcsolt:
-                self._kon[db_nev].detach("kontakt")
+                self[db_nev].detach("kontakt")
     
     @property
     def db_struktura(self):
