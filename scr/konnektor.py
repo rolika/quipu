c7413b26adb331555b85f021c83915c678f5711c
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-03-09 19:27:25 +0100

    adatbázis file-ok létrehozása

--- scr/konnektor.py
+++ scr/konnektor.py
@@ -1,17 +1,41 @@
+"""Adatbázis file-ok létrehozása, megnyitása"""
+
+import pathlib
+import json
+import tamer
+
+
 class Konnektor:
     """Adatbázis kapcsolat gyűjtőosztálya.
-    A vég nélküli paraméterlistákat hivatott kiváltani, egy név alá gyűjtve az összes adatbázis-kapcsolatot.
-    Kezeli az összetett lekérdezéseket, melyeket a tamer.Tamer() nem tud."""
+    A külső használat úgy fog kinézni, hogy pl. kon.projekt.select(...)"""
     def __init__(self, **kwargs) -> None:
         """Konnektor inicializása.
         kwargs: név=konnektor párosok
             A név egy valid Python változó legyen, ami célszerűen azonos az adatbázis filenevekkel.
             A konnektor pedig az adatbázis-file csatlakozásánák hivatkozása.
-        
-        A külső használat úgy fog kinézni, hogy pl. kon.projekt.select(...)
         """
-        for nev, kon in kwargs.items():
-            setattr(self, nev, kon)
+        db_path = pathlib.Path("db/").resolve()
+        self._kon = dict()
+
+        # ha még nem léteznek, létrehozom az adatbázis file-okat
+        with open(pathlib.Path("sql/sql_create.json")) as f:
+            self._db_struktura = json.load(f)
+        
+        for db_nev in self._db_struktura:
+            print("Kapcsolódás adatbázishoz:", db_nev)
+            self._kon[db_nev] = tamer.Tamer(db_path / (db_nev+".db"))
+            if db_nev in ("projekt", "raktar"):
+                kontakt_kapcsolt = self._kon[db_nev].attach(kontakt=str(db_path/"kontakt.db"))
+            else:
+                kontakt_kapcsolt = False
+            for tabla, oszlopok in self._db_struktura[db_nev].items():
+                self._kon[db_nev].create(tabla, **oszlopok)
+            if kontakt_kapcsolt:
+                self._kon[db_nev].detach("kontakt")
+    
+    @property
+    def db_struktura(self):
+        return self._db_struktura
     
     def szemelyhez_rendelt_szervezetek(self, szemelyazonosito):
         self._kon.szemely.execute("""
@@ -27,9 +51,4 @@ class Konnektor:
 
 # az elv tesztelése
 if __name__ == "__main__":
-    kon = Konnektor(szemely="ez lesz a személykonnektor",
-                    szervezet="ő pedig a szervezet-konnektor",
-                    projekt="a projektek konnektora")
-    print(kon.szemely)
-    print(kon.szervezet)
-    print(kon.projekt)
+    kon = Konnektor()
